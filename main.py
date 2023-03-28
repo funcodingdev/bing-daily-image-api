fc433a17924096021f519a07c0718514cbffcb07
FangJu <funcodingdev@foxmail.com>
2023-03-28 21:17:42 +0800

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    response = requests.get("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1")
+    image_url = "https://www.bing.com" + response.json()["images"][0]["url"]
+    return RedirectResponse(url=image_url)
